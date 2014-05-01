8731835b88b2f693d1f226dae4e4357618ee66ee
pssystem <store@perceptionsystem>
2014-05-01 17:30:27 +0530

    theme_pslearning.php

---
+++ lang/en/theme_pslearning.php
@@ -0,0 +1,160 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details
+ *
+ * @package    Theme
+ * @subpackage pslearning
+ * @copyright 2014 onwards Perception System Pvt.Ltd.  (http://www.perceptionsystem.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+/* Core */
+$string['configtitle'] = 'PSlearning';
+$string['pluginname'] = 'PSlearning';
+$string['choosereadme'] = '
+<div class="clearfix">
+<div class="well"> 
+</div></div>';
+
+/* General */
+$string['geneicsettings'] = 'Header settings';
+$string['logo'] = 'Logo';
+$string['logodesc'] = 'Please upload your custom logo here if you want to add it to the header.<br>
+	Logo image should be a PNG files sized 326px × 250px';
+
+$string['headertexttitle'] = 'Header Title';
+$string['headertextdesc'] = 'Whatever you add to this textarea will be displayed on the front page.';
+
+$string['headertextdetail'] = 'Header Details';
+$string['headertextdetaildesc'] = 'Whatever you add to this textarea will be displayed on the front page.';
+
+
+/* Welcome page Setting */
+$string['welcomepage'] = 'Welcome page settings';
+$string['welcometitle'] = 'Title';
+$string['welcometitledesc'] = 'Welcome page title that will be display on the front page';
+
+$string['welcomesubtitle'] = 'Sub-title';
+$string['welcomesubtitledesc'] = 'Welcome page sub-title that will be display on the front page';
+
+$string['welcomecontent'] = 'Description';
+$string['welcomecontentdesc'] = 'Whatever you add to this textarea that will be displayed on the front page.';
+
+$string['pagebackground'] = 'Welcome background image';
+$string['welcomebgimagedesc'] = 'This is the background of the welcome block. Background image should be a PNG files sized 1000px × 334px';
+
+$string['welcomebuttontext'] = 'Link text';
+$string['welcomebuttontextdesc'] = 'Text to appear on the button.';
+$string['welcomebuttonurl'] = 'Link URL';
+$string['welcomebuttonurldesc'] = 'URL the button will point to.';
+
+/* Upcoming course page Setting */
+
+$string['upcourse'] = 'Upcoming course page settings';
+$string['upcourseseparatoricon'] = 'Upcoming course separator icon.';
+$string['upcoursesepicondesc'] = 'Icon should be a PNG files sized 44px × 44px';
+$string['upcoursetitle'] = 'Upcoming title';
+$string['upcoursetitledesc'] = 'Descriptive upcoming title';
+$string['upcoursecontent'] = 'Descriptive title content';
+$string['upcoursecontentdesc'] = 'Enter a descriptive title content for your upcoming course';
+
+
+/* Testimonial page Setting */
+
+$string['testimonial'] = 'Testimonial page settings';
+$string['testimonialseparatoricon'] = 'Testimonial separator icon.';
+$string['testimonialsicondesc'] = 'Icon should be a PNG files sized 44px × 44px';	
+$string['testimonialtitle'] = 'Testimonial title';
+$string['testimonialtitledesc'] = 'Add title that will dispalyed on the top of the testimonial block';
+$string['testimonialseparatorline'] = 'Testimonial slider line.';
+$string['testimonialslinedesc'] = 'Image should be a PNG files sized 316px × 18px';		
+$string['testimonialbackground'] = 'Testimonial background image.';
+$string['testimonialbgdesc'] = 'Background image should be a PNG files sized 1000px × 334px';
+
+		
+
+$string['testimonial1'] = 'TESTIMONIAL ONE';
+$string['testimonial2'] = 'TESTIMONIAL TWO';
+$string['testimonial3'] = 'TESTIMONIAL THREE';
+$string['testimonial4'] = 'TESTIMONIAL FOUR';
+  
+$string['testimonialcontent'] = 'Testimonial content';
+$string['testimonialcontentdesc'] = 'Enter a descriptive content for your testimonial';
+$string['testimonialname'] = 'Name ';
+$string['testimonialnamedesc'] = 'Add name of users or client';
+$string['testimonialcompanyname'] = 'Company name';
+$string['testimonialcnamedesc'] = 'Add Company name that will dispalyed into testimonial block';
+ 
+ 
+ /* Upcoming course page Setting */
+
+$string['purchase'] = 'Purchase theme settings';
+$string['purchaseheading'] = 'Purchase heading';
+$string['purchaseheadingdesc'] = 'Add text for purchase heading that will be displayed on the front page.';
+$string['purchasebtntext'] = 'Purchase link text';
+$string['purchasebtndesc'] = 'Add text to appear on the button.';
+$string['purchasebtnurl'] = 'Purchase link URL';
+$string['purchasebtnurldesc'] = 'Provide purchase link URL';
+
+
+/* Social Networks */
+$string['socialheading'] = 'Social media';
+$string['socialheadingsub'] = 'Engage your users with social networking';
+
+$string['gmail'] = 'Gmail account';
+$string['gmailaddesc'] = 'Enable or disable gmail account';
+$string['gmailurl'] = 'Email URL';
+$string['gmailurldesc'] = 'Enter the URL of your email service. (i.e http://mail.mycollege.ac.uk or http://www.mycollege.edu/mail)';
+$string['gmailicon'] = 'Email icon';
+$string['gmailicondesc']='Social icon.<br>Icon should be a PNG files sized 50px × 50px';
+
+$string['facebook'] = 'Facebook account';
+$string['facebookaddesc'] = 'Enable or disable facebook account';
+$string['facebookurl'] = 'Facebook URL';
+$string['facebookurldesc'] = 'Enter the URL of your facebook service. (i.e http://www.facebook.com/mycollege)';
+$string['facebookicon'] = 'Facebook icon';
+$string['facebookicondesc']='Social icon.<br>Icon should be a PNG files sized 50px × 50px';
+
+$string['twitter'] = 'Twitter account';
+$string['twitteraddesc'] = 'Enable or disable twitter account';
+$string['twitterurl'] = 'Twitter URL';
+$string['twitterurldesc'] = 'Enter the URL of your twitter feed service. (i.e http://www.twitter.com/mycollege)';
+$string['twittericon'] = 'Twitter icon';
+$string['twittericondesc']='Social icon.<br>Icon should be a PNG files sized 50px × 50px';
+
+$string['skype'] = 'Skype account';
+$string['skypeaddesc'] = 'Enable or disable skype account';
+$string['skypeurl'] = 'Skype URL';
+$string['skypeurldesc'] = 'Enter the skype username of your organisations';
+$string['skypeicon'] = 'Skype icon';
+$string['skypeicondesc']='Social icon.<br>Icon should be a PNG files sized 50px × 50px';
+
+/* Footer */
+$string['footer'] = 'Footer setting';
+$string['footnotedesc'] = 'The content from this textarea will be displayed in the footer of every page.';
+$string['projectname'] = 'Compny name';
+$string['projectnamedesc'] = 'That will be displayed in the footer of every page.';
+$string['projecturl'] = 'Website URL';
+$string['projecturldesc'] = 'Provide website link whatever your site.';
+$string['copyrights'] = 'Copyright';
+$string['copyrightsdesc'] = 'That will be displayed in the footer of every page.';
+$string['privacypolicyurl'] = 'Privacy policy URL';
+$string['privacyurldesc'] = 'Provide privacy policy link.';
+
+
